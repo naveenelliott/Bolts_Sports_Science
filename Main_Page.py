bbfe6b6933ef3742e45bce92e83e5388ae0daeed
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-10-28 16:33:10 -0400

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly.express as px
 from GetPlayerTrainingAverages import getTrainingAverages
 
-st.set_page_config(page_title='Bolts Training Reprots', page_icon = 'Training.png')
+st.set_page_config(page_title='Bolts Training Reports', page_icon = 'Training.png')
 
 training_averages = getTrainingAverages()
 
